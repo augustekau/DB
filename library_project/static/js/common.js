9d24ece3170f56e9566c6c2abd99114832fe4a6d
augustekau <83609317+augustekau@users.noreply.github.com>
2021-10-14 21:29:20 +0300

    delete alert

--- library_project/static/js/common.js
+++ library_project/static/js/common.js
@@ -4,7 +4,7 @@ deleteButton.forEach((node) =>
   node.addEventListener("click", function (e) {
     e.preventDefault();
 
-    if (confirm("Ar tikrai norite pašalinti šią kompaniją?"))
+    if (confirm("Are you sure you want to delete this author??"))
       window.location = node.getAttribute("href");
   })
 );
